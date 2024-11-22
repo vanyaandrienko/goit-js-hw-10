38ef4b838afed5783cb96d227e1ed47003394426
vanyaandrienko <vanyaandrienko@gmail.com>
2024-11-22 10:49:59 +0000

    Deploying to gh-pages from @ vanyaandrienko/goit-js-hw-10@29b8bec1fe995f9b7f8a27b1b9cf88b4bea9eb05 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as m}from"./assets/vendor-A92OCY9B.js";const o=document.querySelector(".form");o.addEventListener("submit",e=>{e.preventDefault();const t=o.elements.delay.value,s=o.elements.state.value,r=Number(t);n(r,s).then(i=>{m.success({title:"Success",message:i})}).catch(i=>{m.error({title:"Error",message:i})})});function n(e,t){return new Promise((s,r)=>{setTimeout(()=>{t==="fulfilled"?s(`✅ Fulfilled promise in ${e}ms`):r(`❌ Rejected promise in ${e}ms`)},e)})}
+//# sourceMappingURL=2-snackbar.js.map
